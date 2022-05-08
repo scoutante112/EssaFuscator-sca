f4156a99b2f501d35b5504be401e8bcf5cd2f3b0
Aoun Abu Hassan <80323589+Aoun-Abu-Hassan@users.noreply.github.com>
2022-05-08 03:15:22 +0300

    api update

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
 const saltshani1 = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
+
 class EF {
   /**
-   * @param {*} api_key
+   * @param {string} api_key
  */
   constructor(api_key) {
     this.api_key = api_key;
   }
+  
   /**
    * @param {string} script
  * @param {FileCallback} callback  - You can use filesystem
- * * @param {string} watermark  - Your own watermark
+ * @param {string} watermark  - Your own watermark
+ * @param {string} EncodingType -- arabic / emoji
  */
-  obfuscate(script,watermark,callback){
-    if(!script | !this.api_key) return console.log('Something is missing.....')
+
+
+//add encodign 
+  obfuscate(script,watermark,EncodingType,callback){
+    if(!script | !this.api_key | !EncodingType ) return console.log('Something is missing.....')
       if(script && this.api_key){
-        saltshani1('https://obfuscate.essaprime.xyz/', {
+        saltshani1(`https://api.essafuscator.net/v2/obfuscate/${EncodingType}`, {
         method: "post",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "EssaFuscatorAPIKey":`${this.api_key}`
         },
         body: JSON.stringify({
             script: script,
             watermark: watermark,
-            apikey: this.api_key,
         })
     })
     .then(res => {
+      if(res.status === 200){
         return res.text()
+      }else{
+        throw(new Error('Something went wrong'))
+      }
     })
     .then(async function (response) {
           var file = JSON.parse(response)
